cdcbab10b22826ea982d47db1377128db881ee02
EakenderRana <buntychauhan010000@github.com>
2025-08-13 10:51:15 +0530

    v

--- src/components/common/items/Navitems.jsx
+++ src/components/common/items/Navitems.jsx
@@ -3,9 +3,6 @@ import { navLinks } from "../Helper";
 import Link from "next/link";
 
 function Navitems({ direction = "row", onClick }) {
-  // Click handler
-
-
   return (
     <ul
       className={`flex font-Montserrat font-medium transition-all duration-300 ${
@@ -16,6 +13,7 @@ function Navitems({ direction = "row", onClick }) {
         <li key={index}>
           <Link
             href={link.url}
+            onClick={onClick} // <-- yaha add kiya
             className="nav-link relative text-gray-800 hover:text-[#AD544E] transition-all duration-300"
           >
             {link.name}
